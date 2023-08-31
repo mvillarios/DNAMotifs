a9099b4442850323a8e28557abb249c531bfa682
miosv <mvilla2019@inf.udec.cl>
2023-08-31 16:32:55 -0400

    Cambio a C++

---
+++ Greedy/greedy.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+int greedy(vector<string> s){
+    vector<string> 
+};
---
+++ Greedy/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+// Leer archivo de entrada
+vector<string> read_file(const string& file_name) {
+    vector<string> lines;
+    ifstream file(file_name);
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            lines.push_back(line);
+        }
+        file.close();
+    }
+    return lines;
+}
+
+// Obtener el nombre del archivo de entrada
+string get_file_name(int argc, char* argv[]) {
+    if (argc != 2) {
+        cerr << "Error: Debe ingresar el nombre del archivo de entrada" << endl;
+        exit(1);
+    }
+    return argv[1];
+}
+
+int main(int argc, char* argv[]) {
+    string file_name = get_file_name(argc, argv);
+    vector<string> lines = read_file(file_name);
+    for (const auto& line : lines) {
+        cout << line << endl;
+    }
+    return 0;
+}
