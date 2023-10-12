04e89cbc72295668c90d890bc220901338ed42fb
dimaldonado <102496218+dimaldonado@users.noreply.github.com>
2023-10-12 04:05:06 -0300

    feat: se agrego funciones; metaheuristicas, calculardistancia,

---
+++ metaheuristica/funciones.h
@@ -0,0 +1,16 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <vector>
+#include <string>
+
+typedef std::tuple<int, long long> ResultadoGreedy;
+
+int menorValorDna(const std::vector<int>& count, float alpha);
+std::string greedy(std::vector<std::string> s, float alpha);
+std::vector<std::string> read_file(const std::string& file_name);
+std::string get_file_name(int argc, char* argv[]);
+float get_alpha(int argc, char* argv[]);
+void metaheuristica(std::vector<std::string> s);
+
+#endif  // FUNCIONES_H
