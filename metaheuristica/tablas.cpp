e09bb1eca3231e788e2f570d58b9cac5a9a1dc19
miosv <mvilla2019@inf.udec.cl>
2023-10-13 21:21:14 -0300

    Codigo final antes de las tablas

--- metaheuristica/tablas.cpp
+++ metaheuristica/tablas.cpp
@@ -5,34 +5,63 @@
 #include <tuple>
 #include <cmath>
 #include <chrono>
+#include <filesystem>
+#include <map>
+#include <vector>
+#include <numeric>
 
 using namespace std;
+namespace fs = std::filesystem;
 
 int main(int argc, char* argv[]) {
+    fs::path folder_path = "../Dataset/";
 
     std::srand(std::time(NULL));
 
-    string file_name = get_file_name(argc, argv);
-
-    vector<string> lines = read_file(file_name);
-
     float alpha = get_alpha(argc, argv);
+    int t_limite = get_t_limite(argc, argv);
 
-    std::ofstream file("data.txt", std::ofstream::app);
+    // Usamos un mapa para almacenar los resultados y tiempos por conjunto único de "m" y "l"
+    std::map<std::pair<int, int>, std::tuple<int, long long>> results;
 
-    int inst, m, l;
+    // Recorre todos los archivos en la carpeta
+    for (const auto& entry : fs::directory_iterator(folder_path)) {
+        if (fs::is_regular_file(entry)) {
+            // Obtén el nombre del archivo actual
+            fs::path file_path = entry.path();
+            string file_name = file_path.string();
 
-    extractValues(file_name, inst, m, l);
+            vector<string> lines = read_file(file_name);
+            std::ofstream file("data.txt", std::ofstream::app);
 
-    int t_limite = 60;
-    int tam_string = l;
+            int inst, m, l;
+            extractValues(file_name, inst, m, l);
 
-    std::tuple<int, std::vector<char>> res = greedy(lines, alpha, tam_string);
-    std::tuple<int, long long> res2 = grasp(lines, tam_string, t_limite);
+            std::tuple<int, std::vector<char>> res = greedy(lines, alpha, l);
+            std::tuple<int, long long> res2 = grasp(lines, l, t_limite);
 
-    save_data(file, inst, m, l, std::get<0>(res), std::get<0>(res2));
+            save_data(file, inst, m, l, std::get<0>(res), std::get<0>(res2));
 
+            // Almacenar los resultados en el mapa
+            results[{m, l}] = std::make_tuple(std::get<0>(res), std::get<1>(res2));
 
-    file.close();
+            file.close();
+        }
+    }
+
+    // Imprimir los encabezados
+    cout << "m\tl\tgreedy\tmh\tmhtime" << endl;
+
+    // Iterar a través de los resultados y calcular los promedios
+    for (const auto& entry : results) {
+        int m = entry.first.first;
+        int l = entry.first.second;
+
+        int greedy_result = std::get<0>(entry.second);
+        long long grasp_result = std::get<1>(entry.second);
+
+        cout << m << "\t" << l << "\t" << greedy_result << "\t" << grasp_result << "\t" << grasp_result << endl;
+    }
+
     return 0;
 }
