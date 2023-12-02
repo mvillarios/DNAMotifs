aef47cb9b40d73c7606c8230278df4b972ff735b
miosv <mvilla2019@inf.udec.cl>
2023-12-02 06:21:41 -0300

    ✨ Inicio mh hibrida y arreglos en AG y grasp

--- Funciones/funciones.h
+++ Funciones/funciones.h
@@ -5,17 +5,19 @@
 #include <string>
 #include <unordered_map>
 
+std::tuple<int, std::vector<char>> greedy(std::vector<std::string> s, float alpha, int tam_string);
+std::tuple<int, long long> grasp(std::vector<std::string> s, int tam_string, long long t_limite, int tunning);
+std::tuple<int, long long> genetico(std::vector<std::string> s, int tam_string, int tam_poblacion, float alpha, long long t_limite, bool tunning);
+std::tuple<int, long long> mh_hibrida(std::vector<std::string> s, int tam_string, long long t_limite, int tunning, int tam_poblacion_inicial, float porcentaje_seleccionados, float prob_mutacion);
+
 int menorValorDna(const std::vector<int>& count, float alpha);
 int calcularDistancia(const std::string& str, const std::vector<std::string>& dataset);
-std::tuple<int, std::vector<char>> greedy(std::vector<std::string> s, float alpha, int tam_string);
-std::tuple<int, long long> grasp(std::vector<std::string> s, int tam_string, int t_limite, int tunning);
-std::tuple<int, long long> genetico(std::vector<std::string> s, int tam_string, int tam_poblacion, float alpha, int t_limite, bool tunning);
 
 std::vector<std::string> read_file(const std::string& file_name);
 std::string get_file_name(int argc, char* argv[]);
 int get_tam_string(int argc, char* argv[]);
 float get_alpha(int argc, char* argv[]);
-int get_t_limite(int argc, char* argv[]);
+long long get_t_limite(int argc, char* argv[]);
 int get_tam_poblacion(int argc, char* argv[]);
 bool get_tunning(int argc, char* argv[]);
 int get_m (int argc, char* argv[]);
@@ -27,7 +29,8 @@ void close_data(std::ofstream &file);
 void save_data(std::ofstream &file, int inst, int m, int l, int res1, long long res2);
 void clear_data(std::string file_name);
 
-void allInst (int t_limite, float alpha, int tam_poblacion, bool tunning, int m, int l, int algoritmo);
+void allInst (long long t_limite, float alpha, int tam_poblacion, bool tunning, int m, int l, int algoritmo);
 
+std::string localSearch(const std::string& sol_inicial, const std::vector<std::string>& s);
 
 #endif  // FUNCIONES_H
